5a3d658fedb2101678eee4e739b4e5f65eb29fb2
damians <damiano.gianotti@edu.unito.it>
2021-06-27 12:24:08 +0200

    + Update of test/create of db + Update of .gitignore

--- projects/Relational/relational_test.py
+++ projects/Relational/relational_test.py
@@ -1,16 +1,24 @@
 import time
-from projects.Relational.MariaDBConnector import MariaDBConnector
+from projects.Relational.relationaldbhandler import RelationalDbHandler
 from pathlib import Path
 
 if __name__ == '__main__':
     sentiments = ['anger', 'anticipation', 'disgust', 'fear', 'joy', 'sadness', 'surprise', 'trust']
 
-    db_creator = MariaDBConnector()
+    data_path = Path('../..') / 'data'
+
+    db_creator = RelationalDbHandler()
 
     start = time.time()
+
     print('Creazione del database relazionale.')
-    sql_file_path = Path('../../') / 'data' / 'mariadb.sql'
+    sql_file_path = data_path / 'mariadb.sql'
     db_creator.create(sql_file_path)
+
+    print('Inserimento delle risorse lessicali.')
+    dataset_dir = data_path / 'lexical-resources' / 'Sentiments'
+    db_creator.load_lexical_resources(dataset_dir)
+
     end = time.time()
 
     print('Relazionale: {}'.format(end - start))
