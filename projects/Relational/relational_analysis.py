fd42b182422ae6920e1981f973644db8b74c19ed
Lorenzo Tabasso <lorenzo.tabasso96@gmail.com>
2021-07-17 18:45:50 +0200

    Auto stash before merge of "relational" and "origin/relational"

--- projects/Relational/relational_analysis.py
+++ projects/Relational/relational_analysis.py
@@ -1,12 +1,18 @@
 from pathlib import Path
 from relationaldbhandler import RelationalDbHandler
 from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+from collections import defaultdict
 
 
 def print_and_save_wc(handler, all_sentiments, n_most_presents_token):
     for s in all_sentiments:
         lex_resources = handler.get_all_lex_resources_for_sentiment(s)
+        d = defaultdict(list)
+        d2 = defaultdict(list)
+
         for lr in lex_resources:
             n_lex_words = handler.get_n_lex_words(lr)
             n_twitter_words = handler.get_n_twitter_words(s)
@@ -15,21 +21,45 @@ def print_and_save_wc(handler, all_sentiments, n_most_presents_token):
             perc_presence_lex_res = shared_words / n_lex_words
             perc_presence_twitter = shared_words / n_twitter_words
 
-            print("S: {0}, LR: {1}".format(s, lr))
+            print("Statistics for S: {0}, LR: {1}".format(s, lr))
             print("\tn_lex_words: {0}".format(n_lex_words))
             print("\tn_twitter_words: {0}".format(n_twitter_words))
             print("\tshared_words: {0}".format(shared_words))
             print("\tperc_presence_lex_res: {0:.6f}".format(perc_presence_lex_res))
             print("\tperc_presence_twitter: {0:.6f}\n".format(perc_presence_twitter))
 
-            print(f"Finding first {n_most_presents_token} most present tokens")
-            most_present_token = handler.token_most_present(0, s, n_most_presents_token)
-            
-            wc = WordCloud(background_color='white', width=400, height=200)
-            wc.fit_words(most_present_token)
-            wc.to_file(f'output/wordcloud/cloud_{s}.png')
+            d['sentiment'].append(s)
+            d['lex_resource'].append(lr)
+            d['n_lex_words'].append(n_lex_words)
+            d['n_twitter_words'].append(n_twitter_words)
+            d['shared_words'].append(shared_words)
+            d['perc_presence_lex_res'].append(perc_presence_lex_res)
+            d['perc_presence_twitter'].append(perc_presence_twitter)
 
-            # path_output = Path('.') / 'output '/ 'wordcloud' / f'cloud_{s}.png'
+        print("Showing plot")
+        df = pd.DataFrame(data=d)
+        df.plot(title=s.capitalize(),
+                x='lex_resource',
+                xlabel='Lexical Resource',
+                y=['perc_presence_lex_res', 'perc_presence_twitter'],
+                ylabel='Percentage',
+                kind="bar",
+                rot=0)
+        plt.show()
+        print("\tSaving plot to disk")
+        plt.savefig(f'output/plot/plot_{s}.png')
+        print("\tDone")
+
+        print("Creating word cloud")
+        print(f"\tFinding first {n_most_presents_token} most present tokens")
+        most_present_token = handler.token_most_present(0, s, n_most_presents_token)
+        wc = WordCloud(background_color='white', width=400, height=200)
+        wc.fit_words(most_present_token)
+        wc.to_file(f'output/wordcloud/cloud_{s}.png')
+
+        # path_output = Path('.') / 'output '/ 'wordcloud' / f'cloud_{s}.png'
+        print("\tSaving word cloud to disk")
+        print("\tDone\n")
 
 def build_new_resource(handler: RelationalDbHandler, resource_path: Path) -> None:
     """
