1ccec36de6aee46af0a61af82ad198cdc7d5c2fd
Lorenzo Tabasso <lorenzo.tabasso96@gmail.com>
2021-07-19 19:41:27 +0200

    fixed setup.sh, now works on unix (macos at least))

--- setup.sh
+++ setup.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
+echo "Executing docker-compose"
 docker-compose up -d
+echo "Done"
 
-sleep 2
+seconds=3
 
-cmd="echo 'rs.initiate({_id: \`"mongorsconf\`", configsvr: true, members: [{ _id : 0, host : \`"mongoconf\`" }]})' | mongo"
+echo "Executing needed commands on MongoConf, MongoShard1, MongoShard2"
+sleep $seconds
+
+cmd="echo 'rs.initiate({_id: "mongorsconf", configsvr: true, members: [{ _id : 0, host : "mongoconf" }]})' | mongo"
 docker exec -it mongoconf bash -c $cmd
 
-cmd="echo 'rs.initiate({_id : \`"mongors1\`", members: [{ _id : 0, host : \`"mongoshard1\`" }]})' | mongo"
+cmd="echo 'rs.initiate({_id : "mongors1", members: [{ _id : 0, host : "mongoshard1" }]})' | mongo"
 docker exec -it mongoshard1 bash -c  $cmd
 
-cmd="echo 'rs.initiate({_id : \`"mongors2\`", members: [{ _id : 0, host : \`"mongoshard2\`" }]})' | mongo"
+cmd="echo 'rs.initiate({_id : "mongors2", members: [{ _id : 0, host : "mongoshard2" }]})' | mongo"
 docker exec -it mongoshard2 bash -c  $cmd
 
-sleep 2
+echo "Done"
 
-cmd="echo 'sh.addShard(\`"mongors1/mongoshard1\`")' | mongo"
+echo "Adding shards to MongoRouter"
+sleep $seconds
+
+cmd="echo 'sh.addShard("mongors1/mongoshard1")' | mongo"
 docker exec -it mongorouter bash -c $cmd
 
-cmd="echo 'sh.addShard(\`"mongors2/mongoshard2\`")' | mongo"
+cmd="echo 'sh.addShard("mongors2/mongoshard2")' | mongo"
 docker exec -it mongorouter bash -c $cmd
+
+echo "Done"
 
 # docker-compose down
