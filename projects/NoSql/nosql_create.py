65f9c265ca64fc81077bbe16a8f85e931a282424
Damians <damiano.gianotti@edu.unito.it>
2021-07-19 12:23:38 +0200

    Preprocessing e gestione dei tweets

--- projects/NoSql/nosql_create.py
+++ projects/NoSql/nosql_create.py
@@ -1,11 +1,16 @@
-from pathlib import Path
+import os
 import time
+from pathlib import Path
+from noslq_preproccesing import Preprocessor_NoSql
 from nosqldbhandler import NoSqlDbHandler
 
 if __name__ == "__main__":
     data_path = Path('.') / 'data'
 
     no_sql_handler = NoSqlDbHandler()
+    prep = Preprocessor_NoSql()
+
+    # Risorse lessicali
 
     start = time.time()
 
@@ -13,4 +18,44 @@ if __name__ == "__main__":
     no_sql_handler.load_lexical_resources(dataset_dir)
 
     end = time.time()
-    print('Tempo totale create MongoDB: {:.2f} secondi'.format(end - start))
+    total_time = end - start
+    print('Caricamento risorse compiuto in: {:.2f} secondi'.format(total_time))
+
+    # Tweets
+    dataset_dir = data_path / 'twitter-messages'
+
+    for file_name in os.listdir(dataset_dir):
+        file_path = dataset_dir / file_name
+
+        start = time.time()
+        print(f'Sto lavorando sul file: {file_name}')
+
+        for sentiment in no_sql_handler.get_sentiments():
+            if(sentiment in file_name):
+                current_sentiment_name = sentiment
+                break
+
+        print('Prepocessing delle frasi del sentimento {}'.format(
+            current_sentiment_name))
+
+        with open(file_path, 'r', encoding='utf8') as file:
+            tweets = []
+            count = 1
+
+            for line in file.readlines():
+                # print('{}:\t{}'.format(count, line))
+                count += 1
+                tweets.append(prep.preprocess(line, current_sentiment_name))
+                if count == 1000:
+                    break
+
+            print('Scrittura su database.')
+            no_sql_handler.load_tweets(tweets)
+        
+        end = time.time()
+        print('Processamento del file {} compiuto in {:.2f} secondi'.format(
+            file_name, end - start))
+
+        total_time += end - start
+
+    print('Tempo totale create MongoDB: {:.2f} secondi'.format(total_time))
--- projects/NoSql/nosqldbhandler.py
+++ projects/NoSql/nosqldbhandler.py
@@ -7,7 +7,7 @@ import os
 class Sentiment:
     """
     Classe che rappresenta il singolo sentimento
-    Caraterizzata dal nome_s e da proprio identificatore
+    Caraterizzata dal nome_s e da proprio identificatore univoco
     """
 
     def __init__(self, name: str, id=None) -> None:
@@ -29,7 +29,7 @@ class Lexical_resource:
     Classe che rappresenta la risorsa lessicale
     Caraterizzata dal nome, dal sentimento a cui si riferisce,
     da una lista di parole associate e
-    da un proprio identificatore 
+    da un proprio identificatore univoco
     """
 
     def __init__(self, name: str, sentiment: str, words=None, id=None) -> None:
@@ -63,20 +63,64 @@ class Lexical_resource:
     def add_words(self, new_words: set) -> None:
         """
         Estende la lista di parole con un'ulteriore lista
-        :param new_word: parola
-        :type new_word: str
+        :param: new_word: parola
+        :type: new_word: str
         """
         words = list(new_words)
         self.__words.extend(words)
 
 
+class Tweet:
+    """
+    Classe che rappresenta il singolo tweet, simile a :class:`Lexical_resource`.
+    Caraterizzata dal sentimento a cui si riferisce,
+    da una lista di parole contenute nel tweet
+    da un proprio identificatore univoco
+    """
+
+    def __init__(self, sentiment: str, words=None, id=None) -> None:
+        self.__id = id
+        self.__sentiment = sentiment
+        if words is None:
+            self.__words = []
+        else:
+            self.__words = words
+        self.__id = id
+
+    def to_dict(self) -> dict:
+        representation = {
+            'sentiment': self.__sentiment,
+            'words': self.__words
+        }
+        if self.__id is not None:
+            representation.update({'_id': self.__id})
+
+        return representation
+
+    def add_word(self, new_word: str) -> None:
+        """
+        Aggiunge una sola parola alla lista
+        :param new_word: parola
+        :type new_word: str
+        """
+        self.__words.append(new_word)
+
+    def add_words(self, new_words: list) -> None:
+        """
+        Estende la lista di parole con un'ulteriore lista
+        :param: new_word: parola
+        :type: new_word: str
+        """
+        self.__words.extend(new_words)
+
+
 class NoSqlDbHandler:
     __CLIENT_URL = 'mongodb://127.0.0.1:27017/'
     __DB_NAME = 'tweet_analytics'
-    __COLLECTION_TWEETS = 'tweets'
     __COLLECTION_LEXICAL_RESOURCES = 'lexical_resources'
-    __COLLECTION_COMMON_WORDS = 'common_words'
     __COLLECTION_SENTIMENTS = 'sentiments'
+    __COLLECTION_TWEETS = 'tweets'
+    __COLLECTION_COMMON_WORDS = 'common_words'
 
     def __init__(self):
         """
@@ -90,7 +134,7 @@ class NoSqlDbHandler:
     def load_lexical_resources(self, dataset_dir: Path) -> None:
         """
         Inserimento in mongo dei sentimenti, delle risorse lessicali e relative parole associate
-        tramite l'uso di collezioni
+        tramite l'uso di collezioni.
         Se già presenti nel database vengono eliminate e rimpiazzate
         :param dataset_dir: percorso da cui recuperare i documenti
         :type dataset_dir: Path
@@ -138,6 +182,23 @@ class NoSqlDbHandler:
         # Scrittura su db
         collection.insert_many(map(lambda x: x.to_dict(), sentiments))
 
+    def load_tweets(self, tweets: list) -> None:
+        """
+        Inserimento in mongo dei tweets, già pre processati
+        :param tweets: lista di tweet ripulità
+        :type tweets: list
+        """
+        collection = self.__db[self.__COLLECTION_TWEETS]
+
+        collection.insert_many(map(lambda x: x.to_dict(), tweets))
+
+    def drop_tweets(self) -> None:
+        """
+        Droppa dal db la collezione relativa ai tweets
+        """
+        collection = self.__db[self.__COLLECTION_TWEETS]
+        collection.drop()
+
     def get_sentiments(self) -> list:
         """
         :return: Restituisce la lista dei sentimenti 
@@ -148,7 +209,7 @@ class NoSqlDbHandler:
 
         for x in collection.find():
             sentiments.append(x['name'])
-        
+
         return sentiments
 
     def get_lexical_resources(self) -> List[List]:
